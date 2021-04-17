93ed0b7a138cf548113eef9951b673fe47e5c134
Javier Perez <javier21perez98@gmail.com>
2021-04-17 10:52:25 -0700

    update bug

--- script/index.js
+++ script/index.js
@@ -1,18 +1,3 @@
-const express = require("express");
-const path = require("path");
-
-const app = express();
-const PORT = process.env.PORT || 3001;
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-app.get("/", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
-
-app.get("/notes", (req, res) =>
-	res.sendFile(path.join(__dirname, "notes.html"))
-);
-
 let noteTitle;
 let noteText;
 let saveNoteBtn;
@@ -195,5 +180,3 @@ if (window.location.pathname === "/notes") {
 }
 
 getAndRenderNotes();
-
-app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
