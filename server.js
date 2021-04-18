4c94d229152589fe3dcc027e61b320ad6c59bc7e
Javier Perez <javier21perez98@gmail.com>
2021-04-18 13:39:22 -0700

    updated function

--- server.js
+++ server.js
@@ -9,50 +9,41 @@ app.use(express.json());
 app.use(express.static("public"));
 app.use(express.static("db"));
 
-app.get("/", (req, res) =>
-	res.sendFile(path.join(__dirname, "public/index.html"))
-);
-
 app.get("/notes", (req, res) =>
 	res.sendFile(path.join(__dirname, "public/notes.html"))
 );
 
-app.get("/api/notes", (req, res) =>
-	res.sendFile(path.join(__dirname, "/db/db.json"))
-);
-
-app.post("/api/notes", (req, res) => {
+app.get("/api/notes", (req, res) => {
+	res.sendFile(path.join(__dirname, "/db/db.json"));
 	fs.readFile(__dirname + "/db/db.json", "utf8", (error, listNotes) => {
 		if (!error) {
 			listNotes = JSON.parse(listNotes);
-
-			const newNote = {
-				title: req.body.title,
-				text: req.body.text,
-				id: listNotes.length + 1,
-			};
-			const userNote = listNotes.concat(newNote);
-
-			fs.writeFile(
-				__dirname + "/db/db.json",
-				JSON.stringify(userNote),
-				function (error, data) {
-					if (error) {
-						return error;
-					}
-					res.json(userNote);
-				}
-			);
 		} else {
 			return console.log(error);
 		}
 	});
 });
 
-app.get("/api/notes", (req, res) => {
+app.post("/api/notes", (req, res) => {
 	fs.readFile(__dirname + "/db/db.json", "utf8", (error, listNotes) => {
 		if (!error) {
 			listNotes = JSON.parse(listNotes);
+			listNotes.push({
+				title: req.body.title,
+				text: req.body.text,
+				id: listNotes.length + 1,
+			});
+			fs.writeFile(
+				__dirname + "/db/db.json",
+				JSON.stringify(listNotes),
+				function (error, data) {
+					if (!error) {
+						res.json(listNotes);
+					} else {
+						return error;
+					}
+				}
+			);
 		} else {
 			return console.log(error);
 		}
